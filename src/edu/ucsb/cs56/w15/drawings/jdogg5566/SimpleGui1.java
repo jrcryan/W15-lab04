902407955729ec6e26cbdc44b07bd75d5958f731
Jonathan Easterman <jonathaneasterman@gmail.com>
2015-02-02 13:16:14 -0800

    added updated version of SimpleGUI1 to my src directory - done with Step 5

--- src/edu/ucsb/cs56/w15/drawings/jdogg5566/SimpleGui1.java
+++ src/edu/ucsb/cs56/w15/drawings/jdogg5566/SimpleGui1.java
@@ -18,10 +18,13 @@ public class SimpleGui1 {
 
     public static void main (String[] args) {
 	JFrame frame = new JFrame() ;
-	JButton button = new JButton("click me") ;
+	JButton button = new JButton("click me you hairy beast") ;
 	frame. setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE) ;
 	frame. getContentPane() . add(button) ;
 	frame. setSize(300,300) ;
 	frame. setVisible(true) ;
+    java.awt.Color myColor = new java.awt.Color(0,225, 225);   // R, G, B values.
+    button.setBackground(myColor);
+    button.setOpaque(true);
     }
 }
