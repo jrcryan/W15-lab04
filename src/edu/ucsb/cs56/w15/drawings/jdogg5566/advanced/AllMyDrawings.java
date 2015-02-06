9d9992d78f281476338536bf91d0661f4d714029
Jonathan Easterman <jonathaneasterman@gmail.com>
2015-02-06 13:58:06 -0800

    Finished the basic Sweatshirt class. Also modified drawPicture3() and drawPicture1() methods to draw some sweatshirts

--- src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/AllMyDrawings.java
@@ -19,32 +19,32 @@ import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
 /**
  * A class with static methods for drawing various pictures
  * 
- * @author Phill Conrad 
- * @version for CS10, lab06, Spring 2009
+ * @author Jonathan Easterman
+ * @version for CS56, lab04, Winter 2015
  */
 
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a sweatshirts 
      */
 
     public static void drawPicture1(Graphics2D g2) {
 
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Sweatshirt s1 = new Sweatshirt(250,150,200,120);
+	g2.setColor(Color.CYAN); g2.draw(s1);
 	
-	// Make a black house that's half the size, 
+	// Make a black sweatshirt that's half the size, 
 	// and moved over 150 pixels in x direction
 
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape s2 = ShapeTransforms.scaledCopyOfLL(s1,0.5,0.5);
+	s2 = ShapeTransforms.translatedCopyOf(s2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(s2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a sweatshirt that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	s2 = ShapeTransforms.scaledCopyOfLL(s2,4,4);
+	s2 = ShapeTransforms.translatedCopyOf(s2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -56,21 +56,21 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(s2); 
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	//HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
+	//HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+	//g2.draw(hw1);
+	//g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("A few sweatshirts by Jonathan Easterman", 20,20);
     }
 
 
@@ -143,7 +143,7 @@ public class AllMyDrawings
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("Red and blue sweatshirts by Jonathan Easterman", 20,20);
 
 	
 	// Draw some coffee cups.
@@ -154,8 +154,15 @@ public class AllMyDrawings
        g2.setColor(Color.RED);     g2.draw(large);
        g2.setColor(Color.GREEN);   g2.draw(smallCC);*/
 
-       Sweatshirt s = new Sweatshirt(20,20,100,60);
-       g2.draw(s);
+       Sweatshirt sBig = new Sweatshirt(250,150,200,120);
+       Sweatshirt sSmall = new Sweatshirt(50, 50, 67, 40);
+       g2.setColor(new Color(0x002FA7));
+       g2.fill(sBig);
+       g2.draw(sBig);
+
+       g2.setColor(Color.RED);
+       g2.fill(sSmall);
+       g2.draw(sSmall);
 
        
     }
--- src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/Sweatshirt.java
+++ src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/Sweatshirt.java
@@ -25,31 +25,32 @@ public class Sweatshirt extends GeneralPathWrapper implements Shape {
 /**
        Constructor
 
-       @param x x coord of center of torso of sweatshirt
-       @param y y coord of center of torso of sweatshirt
+       @param x x coord of top-left of torso of sweatshirt
+       @param y y coord of top-left of torso of sweatshirt
 	@param tHeight the height of the torso shape, arms and hood will be scaled accordingly
 	@param tWidth the width of the torso shape, arms and hood will be scaled accordingly
 */
 
     public Sweatshirt (double x, double y, double tHeight, double tWidth) {
-        double armY = y + .5*tHeight; // y coord of the top of the arm
-        double rightArmX = x + .5*tWidth; // x coord of the right arm
-        double leftArmx = x - .5*tWidth; // x coord of the left arm
-        double circleCenterX = x;
-        double circleCenterY = y - 1.25*tHeight;
-        double circleRadius = tWidth * .25;
+        double armY = y; // y coord of the top of the arm
+        double rightArmX = x+tWidth; // x coord of the top-left of right arm
+        double leftArmx = x - 1.67*tWidth; // x coord of the top-left of left arm
+
+        double circleX = x; // x coord of top left of circle
+        double circleY = y - .75*tWidth; // y coord of top left of circle
+        double circleDiameter = tWidth; // Diameter of circle is equal to width of torso
 
         Rectangle2D.Double torso =
             new Rectangle2D.Double(x,y,tWidth,tHeight);
 
         Rectangle2D.Double leftArm = 
-            new Rectangle2D.Double();
+            new Rectangle2D.Double(leftArmx,armY,tHeight,.25*tWidth);
 
         Rectangle2D.Double rightArm = 
-            new Rectangle2D.Double();
+            new Rectangle2D.Double(rightArmX,armY,tHeight,.25*tWidth);
 
         Ellipse2D.Double hood = 
-            new Ellipse2D.Double(circleCenterX,circleCenterY,circleRadius,circleRadius);
+            new Ellipse2D.Double(circleX,circleY,circleDiameter,circleDiameter);
 
         GeneralPath wholeSweatshirt = this.get();
         wholeSweatshirt.append(torso, false);
@@ -57,7 +58,6 @@ public class Sweatshirt extends GeneralPathWrapper implements Shape {
         wholeSweatshirt.append(rightArm, false);
         wholeSweatshirt.append(hood, false);
 
-        this.set(wholeSweatshirt);
     }
 
 }
