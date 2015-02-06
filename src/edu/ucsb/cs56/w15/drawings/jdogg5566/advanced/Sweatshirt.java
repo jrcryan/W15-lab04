beec14f8d54dd8c56b4b826cec98a8ca9446cafc
Jonathan Easterman <jonathaneasterman@gmail.com>
2015-02-05 18:06:38 -0800

    added Sweatshirt.java and Sweatshirt WithLogo.java, started on constructor of Sweatshirt.java

---
+++ src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/Sweatshirt.java
@@ -0,0 +1,43 @@
+package edu.ucsb.cs56.w15.drawings.jdogg5566.advanced;
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+import java.awt.geom.AffineTransform; // translation, rotation, scale
+import java.awt.Shape; // general class for shapes
+
+// all imports below this line needed if you are implementing Shape
+import java.awt.geom.Point2D; 
+import java.awt.geom.Line2D; 
+import java.awt.geom.Rectangle2D;
+import java.awt.Rectangle;
+import java.awt.geom.PathIterator;
+import java.awt.geom.AffineTransform;
+
+import edu.ucsb.cs56.w15.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
+
+
+/**
+   A vector drawing of a sweatshit that implements
+   the Shape interface, and so can be drawn, as well as
+   rotated, scaled, etc.
+      
+   @author Jonathan Easterman 
+   @version for CS56, Winter 15, UCSB
+   
+*/
+
+public class Sweatshirt extends GeneralPathWrapper implements Shape {
+
+/**
+       Constructor
+
+       @param x x coord of center of torso of sweatshirt
+       @param y y coord of center of torso of sweatshirt
+	@param tHeight the height of the torso shape, arms and hood will be scaled accordingly
+	@param tWidth the width of the torso shape, arms and hood will be scaled accordingly
+*/
+
+	public Sweatshirt (double x, double y, double tHeight, double tWidth) {
+		
+	}
+
+}
