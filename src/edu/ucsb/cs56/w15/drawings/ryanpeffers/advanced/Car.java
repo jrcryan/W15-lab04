2de9e64db4999ef79fc4f844913c77e8913a49a8
Ryan Peffers <ryanpeffers@umail.ucsb.edu>
2015-02-17 14:01:12 -0800

    In middle of assigning lengths for parts of car

---
+++ src/edu/ucsb/cs56/w15/drawings/ryanpeffers/advanced/Car.java
@@ -0,0 +1,81 @@
+package edu.ucsb.cs56.w15.drawings.ryanpeffers.advanced;
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+import java.awt.geom.AffineTransform; // translation, rotation, scale
+import java.awt.Shape; // general class for shapes
+
+// all imports below this line needed if you are implementing Shape
+import java.awt.geom.Point2D; 
+import java.awt.geom.Line2D; 
+import java.awt.geom.Rectangle2D;
+import java.awt.Rectangle;
+import java.awt.geom.PathIterator;
+import java.awt.geom.AffineTransform;
+
+import edu.ucsb.cs56.w15.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
+
+/**
+   A vector drawing of a car that implements
+   the Shape interface, and so can be drawn, as well as
+   rotated, scaled, etc.
+      
+   @author Ryan Peffers
+   @version for CS56, Winter 15, UCSB
+   
+*/
+public class Car extends GeneralPathWrapper implements Shape
+{
+    /**
+       Constructor
+
+       @param x x coord of lower left corner of car
+       @param y y coord of lower left corner of car
+       @param width width of the car
+       @param height of car
+     */
+    public House(double x, double y, double width, double height)
+    {
+    
+        // Rather than having to scale at the end, we can just
+        // draw things the right way to begin with, using the
+        // x, y, width and height.   If you haven't already
+        // hard coded a particular drawing, this may be an easier
+        // way.
+        
+	//        double firstStoryHeight = .75 * height;
+	//        double roofHeight = height - firstStoryHeight;
+        double underSide1 = 0.2 * length;
+	double bumper2 = 0.66 * height;
+	double trunkLength3 = underSide1;
+	double rearWindow4 = 1.4142 * 0.34 * height; // length of 45 degree angle windows
+	double roof5 = 
+
+	//        double firstStoryUpperLeftY = y + roofHeight;
+        
+        // Make the first story
+        
+        Rectangle2D.Double firstStory = 
+            new Rectangle2D.Double(x, firstStoryUpperLeftY ,
+                          width, firstStoryHeight);
+                          
+        // make the roof.   Remember that y goes DOWN the page,
+        // so we ADD to y to get a "lower" value on the screen
+        
+        Line2D.Double leftRoof = 
+            new Line2D.Double (x, y + roofHeight,
+                               x + width/2.0, y);
+                               
+        Line2D.Double rightRoof =
+            new Line2D.Double (x + width/2.0, y,
+                               x + width, y + roofHeight);
+
+        // put the whole house together
+       
+        GeneralPath wholeHouse = this.get();
+        wholeHouse.append(firstStory, false);
+        wholeHouse.append(leftRoof, false);
+        wholeHouse.append(rightRoof, false); 
+        
+    }
+
+}
