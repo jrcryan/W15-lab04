cf0e78970ba6bdb54114e4d5b80dd8ef53d2a7a6
Ishi von Meier <vonmeier@umail.ucsb.edu>
2015-02-19 14:10:13 -0800

    Added TreeWithBalls class and changed AllMyDrawings to implement new class

--- src/edu/ucsb/cs56/w15/drawings/vonmeier/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w15/drawings/vonmeier/advanced/AllMyDrawings.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs56.w15.drawings.vonmeier.advanced;
 
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;  // single lines
+import java.awt.geom.Arc2D; //Arcs
 import java.awt.geom.Ellipse2D;  // ellipses and circles
 import java.awt.geom.Rectangle2D; // for the bounding box
 import java.awt.Rectangle;  // squares and rectangles
@@ -32,7 +33,7 @@ public class AllMyDrawings
     public static void drawPicture1(Graphics2D g2) {
 
 	Tree t1 = new Tree(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(t1);
+	g2.setColor(Color.GREEN); g2.draw(t1);
 	
 	// Make a black tree that's half the size, 
 	// and moved over 150 pixels in x direction
@@ -53,18 +54,21 @@ public class AllMyDrawings
 	// #002FA7 is "International Klein Blue" according to Wikipedia
 	// In HTML we use #, but in Java (and C/C++) its 0x
 	
-	Stroke orig=g2.getStroke();
-	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
+	
+	g2.setColor(new Color(0x458B00)); 
 	g2.draw(t2); 
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	TreeWithBalls tb1 = new TreeWithBalls(50,350,75,150);
+	TreeWithBalls tb2 = new TreeWithBalls(200,350,250,250);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+	g2.draw(tb1);
+	
+	Stroke orig=g2.getStroke();
+	g2.setStroke(thick);
+	
+	g2.setColor(new Color(0x78AB46)); g2.draw(tb2);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
@@ -80,32 +84,32 @@ public class AllMyDrawings
 
 	// Draw some coffee cups.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+	TreeWithBalls large = new TreeWithBalls(100,50,225,150);
+	TreeWithBalls smallCC = new TreeWithBalls(20,50,40,30);
+	TreeWithBalls tallSkinny = new TreeWithBalls(20,150,20,40);
+	TreeWithBalls shortFat = new TreeWithBalls(20,250,40,20);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
 	g2.setColor(Color.GREEN);   g2.draw(smallCC);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Tree t1 = new Tree(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(t1);
 	
-	// Make a black house that's half the size, 
+	// Make a black tree that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape t2 = ShapeTransforms.scaledCopyOfLL(t1,0.5,0.5);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(t2);
 	
 	// Here's a house that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	t2 = ShapeTransforms.scaledCopyOfLL(t2,4,4);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
 	
 	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
+	Stroke thick = new BasicStroke (2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
 	
 	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
 	// #002FA7 is "International Klein Blue" according to Wikipedia
@@ -114,26 +118,26 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(t2); 
 	
-	// Draw two houses with Windows
+	// Draw two trees with ornaments
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	TreeWithBalls tb1 = new TreeWithBalls(50,350,60,90);
+	TreeWithBalls tb2 = new TreeWithBalls(200,350,200,100);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); 
+	g2.draw(tb1);
+	g2.setColor(new Color(0x4A7023)); 
 
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	// Rotate the second Tree 45 degrees around its center.
+	Shape tb3 = ShapeTransforms.rotatedCopyOf(tb2, Math.PI/4.0);
 
-	g2.draw(hw3);
+	g2.draw(tb3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of Trees and some with Ornaments by Ishi von Meier", 20,20);
     }
   
     /** Draw a different picture with a few houses and coffee cups
@@ -143,16 +147,16 @@ public class AllMyDrawings
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("A bunch of Trees by Ishi von Meier", 20,20);
 
 	
 	// Draw some coffee cups.
 	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+       Tree large = new Tree(100,50,225,150);
+       Tree smallT = new Tree(20,50,40,30);
        
        g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
+       g2.setColor(Color.GREEN);   g2.draw(smallT);
        
        
     }
