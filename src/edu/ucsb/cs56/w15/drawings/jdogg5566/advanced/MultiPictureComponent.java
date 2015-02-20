154c9aed67a348691c91833df884b05ebda539ec
Jonathan Easterman <jonathaneasterman@gmail.com>
2015-02-19 22:28:46 -0800

    Modified comments in MultiPicture* files

--- src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/MultiPictureComponent.java
+++ src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/MultiPictureComponent.java
@@ -21,10 +21,10 @@ import java.awt.Stroke; // Stroke interface
 import java.awt.BasicStroke; // class that implements stroke
 
 /**
-   A component that draws a Picture by Phill Conrad
+   A component that draws a Picture by Jonathan Easterman
    
-   @author Phill Conrad (original drawing)
-   @version CS56, Spring 2012, UCSB
+   @author Jonathan Easterman (new drawing)
+   @version CS56, Winter 2015, UCSB
 
    
 */
--- src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/MultiPictureViewer.java
+++ src/edu/ucsb/cs56/w15/drawings/jdogg5566/advanced/MultiPictureViewer.java
@@ -6,8 +6,8 @@ import javax.swing.JFrame;
  *  just three simple Java graphics objects, namely
  *  Rectangle, Line2D.Double, Ellipse2D.Double
  *  
- * @author P. Conrad 
- * @version for UCSB CS56, S12, 02/09/2011
+ * @author Jonathan Easterman
+ * @version for UCSB CS56, W15, 02/19/2015
  */
 
 public class MultiPictureViewer
@@ -31,7 +31,7 @@ public class MultiPictureViewer
        frame.setSize(640,480); // @@@ MODIFY THIS LINE IF YOU LIKE
        
        // Set your own title
-       frame.setTitle("Phill's First Drawing"); // @@@ MODIFY THIS LINE
+       frame.setTitle("Jonathan's first drawing"); // @@@ MODIFY THIS LINE
        
        // Always do this so that the red X (or red circle) works
        // to close the window. 
